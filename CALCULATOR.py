8b34ac6c1f7becdf1be96910f5c71980fbda05ae
Cyber-AlbSecOP <cyber-albsecop@proton.me>
2023-12-30 13:52:48 +0100

    Update 30/12/2023

--- CALCULATOR.py
+++ CALCULATOR.py
@@ -1,67 +1,103 @@
+#LISTA OPERAZIONI
+#creo la lista delle operazioni disponibili e la stampo
+lista_op = ["addizione", "sottrazione", "moltiplicazione", "divisione", "esponenza", "binario"]
+print("Le operazioni disponibili sono:", lista_op)
+
+
+#INPUT OPERAZIONE
+#chiedo all'utente quale operazione vuole fare
+op_scelta = input("Che operazione vuoi fare?    ")
+
+
 #ADDIZIONE
-add_val_1 = float(input("Scrivi un numero:    "))
-#definisco prima variabile = int / float
-add_val_2 = float(input("Scrivi un altro numero:    "))
-#definisco seconda variabile = int / float
-risultato_add = add_val_1 + add_val_2
-print(add_val_1, "+", add_val_2, "=", risultato_add)
-#eseguo addizione e stampo risultato
+#se l'utente sceglie addizione, provo a chiedere due valori
+if op_scelta == "addizione":
+    try:
+        add_val_1 = float(input("Scrivi un numero:    "))
+        add_val_2 = float(input("Scrivi un altro numero:    "))
+#se l'utente inserisce un valore non numerico, stampo un messaggio di errore
+    except ValueError as e:
+        print("I valori inseriti non sono numeri!")
+#se l'utente inserisce due valori numerici, stampo il risultato
+    else:
+        risultato_add = add_val_1 + add_val_2
+        print(add_val_1, "+", add_val_2, "=", risultato_add)
+
 
 #SOTTRAZIONE
-sott_val_1 = float(input("Scrivi un numero:    "))
-#definisco prima variabile = int / float
-sott_val_2 = float(input("Scrivi un altro numero:    "))
-#definisco seconda variabile = int / float
-risultato_sott = sott_val_1 - sott_val_2
-print(sott_val_1, "-", sott_val_2, "=", risultato_sott)
-#eseguo sottrazione e stampo risultato
+#se l'utente sceglie sottrazione, provo a chiedere due valori
+elif op_scelta == "sottrazione":
+    try:
+        sott_val_1 = float(input("Scrivi un numero:    "))
+        sott_val_2 = float(input("Scrivi un altro numero:    "))
+#se l'utente inserisce un valore non numerico, stampo un messaggio di errore
+    except ValueError as e:
+        print("I valori inseriti non sono numeri!")
+    else:
+        risultato_sott = sott_val_1 - sott_val_2
+        print(sott_val_1, "-", sott_val_2, "=", risultato_sott)
+
 
 #MOLTIPLICAZIONE
-molt_val_1 = float(input("Scrivi un numero:    "))
-#definisco prima variabile = int / float
-molt_val_2 = float(input("Scrivi un altro numero:    "))
-#definisco seconda variabile = int / float
-risultato_molt = molt_val_1 * molt_val_2
-print(molt_val_1, "*", molt_val_2, "=", risultato_molt)
-#eseguo moltiplicazione e stampo risultato
+#se l'utente sceglie moltiplicazione, provo a chiedere due valori
+elif op_scelta == "molteplicazione":
+    try:
+        molt_val_1 = float(input("Scrivi un numero:    "))
+        molt_val_2 = float(input("Scrivi un altro numero:    "))
+#se l'utente inserisce un valore non numerico, stampo un messaggio di errore
+    except ValueError as e:
+        print("I valori inseriti non sono numeri!")
+#se l'utente inserisce due valori numerici, stampo il risultato
+    else:
+        risultato_molt = molt_val_1 * molt_val_2
+        print(molt_val_1, "*", molt_val_2, "=", risultato_molt)
+
 
 #DIVISIONE
-div_val_1 = input("Scrivi un numero:    ")
-#definisco prima variabile = int / float
-div_val_2 = input("Scrivi un altro numero:    ")
-#definisco seconda variabile = int / float
-try:
-    div_val_1_float = float(div_val_1)
-    div_val_2_float = float(div_val_2)
-except ValueError as e:
-    print("I valori inseriti non sono numeri!")
-else:
-    risultato_div = div_val_1 / div_val_2
-    print(div_val_1, "/", div_val_2, "=", risultato_div)
-#eseguo divisione e stampo risultato
+#se l'utente sceglie divisione, provo a chiedere due valori
+elif op_scelta == "divisione":
+    try:
+        div_val_1 = float(input("Scrivi un numero:    "))
+        div_val_2 = float(input("Scrivi un altro numero:    "))
+#se l'utente inserisce un valore non numerico, stampo un messaggio di errore
+    except ValueError as e:
+        print("I valori inseriti non sono numeri!")
+#se l'utente inserisce due valori numerici, stampo il risultato
+    else:
+        risultato_div = div_val_1 / div_val_2
+        print(div_val_1, "/", div_val_2, "=", risultato_div)
+
 
 #ESPONENZA
-esp_val_1 = input("Scrivi un numero:    ")
-#definisco prima variabile = int / float
-esp_val_2 = input("Scrivi un altro numero:    ")
-#definisco seconda variabile = int / float
-try:
-    esp_val_1_float = float(esp_val_1)
-    esp_val_2_float = float(esp_val_2)
-except ValueError as e:
-    print("I valori inseriti non sono numeri!")
-else:
-    risultato_esp = esp_val_1 ** esp_val_2
-    print(esp_val_1, "**", esp_val_2, "=", risultato_esp)
-#eseguo esponenza e stampo risultato
+#se l'utente sceglie esponenza, provo a chiedere due valori
+elif op_scelta == "esponenza":
+    try:
+        esp_val_1 = float(input("Scrivi un numero:    "))
+        esp_val_2 = float(input("Scrivi un altro numero:    "))
+#se l'utente inserisce un valore non numerico, stampo un messaggio di errore
+    except ValueError as e:
+        print("I valori inseriti non sono numeri!")
+#se l'utente inserisce due valori numerici, stampo il risultato    
+    else:
+        risultato_esp = esp_val_1 ** esp_val_2
+        print(esp_val_1, "^", esp_val_2, "=", risultato_esp)
 
-#CONVERSIONE IN BINARIO
-conv_bin_num = (input("Inserisci un numero intero:    "))
-#definisco numero intero
-try:
-    conv_bin_num_int = int(conv_bin_num)
-except ValueError as e:
-    print("Il valore inserito non è un numero intero!") 
+
+#BINARIO
+#se l'utente sceglie binario, provo a chiedere un valore
+elif op_scelta == "binario":
+    try:
+        bin_val_1 = int(input("Scrivi un numero:    "))
+#se l'utente inserisce un valore non numerico, stampo un messaggio di errore
+    except ValueError as e:
+        print("I valori inseriti non sono numeri!")
+#se l'utente inserisce un valore numerico, stampo il risultato
+    else:
+        risultato_bin = bin(bin_val_1)
+        print(bin_val_1, "=", risultato_bin)
+
+
+#INPUT NON VALIDO
+#se l'utente inserisce un input non valido, stampo un messaggio di errore
 else:
-    print(conv_bin_num_int, "=", bin(conv_bin_num_int))
-#converto intero in binario e stampo
+    print("L'input inserito non è valido!")
--- TESTER.py
+++ TESTER.py
@@ -1,28 +1,58 @@
-esp_val_1 = input("Scrivi un numero:    ")
-#definisco prima variabile = int / float
-esp_val_2 = input("Scrivi un altro numero:    ")
-#definisco seconda variabile = int / float
-try:
-    esp_val_1_float = float(esp_val_1)
-    esp_val_2_float = float(esp_val_2)
-except ValueError as e:
-    print("Il valore inserito non è un numero!")
+lista_op = ["addizione", "sottrazione", "moltiplicazione", "divisione", "esponenza", "binario"]
+print("Le operazioni disponibili sono:", lista_op)
+op_scelta = input("Che operazione vuoi fare?    ")
+if op_scelta == "addizione":
+    try:
+        add_val_1 = float(input("Scrivi un numero:    "))
+        add_val_2 = float(input("Scrivi un altro numero:    "))
+    except ValueError as e:
+        print("I valori inseriti non sono numeri!")
+    else:
+        risultato_add = add_val_1 + add_val_2
+        print(add_val_1, "+", add_val_2, "=", risultato_add)
+elif op_scelta == "sottrazione":
+    try:
+        sott_val_1 = float(input("Scrivi un numero:    "))
+        sott_val_2 = float(input("Scrivi un altro numero:    "))
+    except ValueError as e:
+        print("I valori inseriti non sono numeri!")
+    else:
+        risultato_sott = sott_val_1 - sott_val_2
+        print(sott_val_1, "-", sott_val_2, "=", risultato_sott)
+elif op_scelta == "molteplicazione":
+    try:
+        molt_val_1 = float(input("Scrivi un numero:    "))
+        molt_val_2 = float(input("Scrivi un altro numero:    "))
+    except ValueError as e:
+        print("I valori inseriti non sono numeri!")
+    else:
+        risultato_molt = molt_val_1 * molt_val_2
+        print(molt_val_1, "*", molt_val_2, "=", risultato_molt)
+elif op_scelta == "divisione":
+    try:
+        div_val_1 = float(input("Scrivi un numero:    "))
+        div_val_2 = float(input("Scrivi un altro numero:    "))
+    except ValueError as e:
+        print("I valori inseriti non sono numeri!")
+    else:
+        risultato_div = div_val_1 / div_val_2
+        print(div_val_1, "/", div_val_2, "=", risultato_div)
+elif op_scelta == "esponenza":
+    try:
+        esp_val_1 = float(input("Scrivi un numero:    "))
+        esp_val_2 = float(input("Scrivi un altro numero:    "))
+    except ValueError as e:
+        print("I valori inseriti non sono numeri!")
+    else:
+        risultato_esp = esp_val_1 ** esp_val_2
+        print(esp_val_1, "^", esp_val_2, "=", risultato_esp)
+elif op_scelta == "binario":
+    try:
+        bin_val_1 = int(input("Scrivi un numero:    "))
+    except ValueError as e:
+        print("I valori inseriti non sono numeri!")
+    else:
+        risultato_bin = bin(bin_val_1)
+        print(bin_val_1, "=", risultato_bin)
 else:
-    risultato_esp = esp_val_1 ** esp_val_2
-    print(esp_val_1, "**", esp_val_2, "=", risultato_esp)
-#eseguo esponenza e stampo risultato
-    
-#DIVISIONE
-div_val_1 = input("Scrivi un numero:    ")
-#definisco prima variabile = int / float
-div_val_2 = input("Scrivi un altro numero:    ")
-#definisco seconda variabile = int / float
-try:
-    div_val_1_float = float(div_val_1)
-    div_val_2_float = float(div_val_2)
-except ValueError as e:
-    print("I valori inseriti non sono numeri!")
-else:
-    risultato_div = div_val_1 / div_val_2
-    print(div_val_1, "/", div_val_2, "=", risultato_div)
-#eseguo divisione e stampo risultato
+    print("L'input inserito non è valido!")
